0f966c6689d65cf37f4f6a20d91c21a8c3b87e4d
Isabelly Dias <isabellydias29@gmail.com>
2020-12-26 21:45:41 -0300

    Criacao da interface da aplicacao para envio de email

---
+++ src/main/java/br/com/alura/escola/infra/indicacao/EnviarEmailComJavaMail.java
@@ -0,0 +1,11 @@
+package br.com.alura.escola.infra.indicacao;
+
+import br.com.alura.escola.aplicacao.indicacao.EnviarEmailIndicacao;
+import br.com.alura.escola.dominio.aluno.Aluno;
+
+public class EnviarEmailComJavaMail implements EnviarEmailIndicacao {
+    @Override
+    public void enviarPara(Aluno indicado) {
+        
+    }
+}
